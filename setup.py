d9d849861dafbfc8282500453d45b75bcb3d390b
Shagun Agarwal <shagun.1994@gmail.com>
2025-05-01 10:27:05 +0200

    build: add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FSSLibrary',  # Replace with your package name
+    version='0.1.0',
+    description='Set of python library made specifically for Floating and Submerged Structures course.',
+    author='Shagun Agarwal, Oriol Colomés',
+    author_email='shagun.1995@gmail.com, j.o.colomesgene@tudelft.nl',
+    url='https://github.com/CMOE-TUDelft/FSSLibrary',
+    packages=find_packages(),  # Automatically finds all packages/modules
+    install_requires=[
+        # List dependencies here, e.g.:
+        'requests',
+        'numpy',
+        'math',
+        'scipy',
+        'typing',
+        'copy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Or your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
